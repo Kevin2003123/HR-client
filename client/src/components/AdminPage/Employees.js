59c0f1da3b55867fdc357e2e6709aa02a2ada4f1
Kevin Fangio Reyes Lopez <kevinreyes005@gmail.com>
2020-11-01 22:14:09 -0400

    commit30

--- client/src/components/AdminPage/Employees.js
+++ client/src/components/AdminPage/Employees.js
@@ -108,11 +108,11 @@ const Employees = ({
   }
 
   return loading ? (
-    <div className='container-fluid vh-100'>
+    <div className='container-fluid vh-100 '>
       <Spinner />
     </div>
   ) : (
-    <div className='container-fluid vh-100 m-0 p-0 w-100 '>
+    <div className='container-fluid vh-100 m-0 p-0 w-100 container-small'>
       <div className='row row-cols-1 p-0'>
         <div className='col p-0 w-100 '>
           <Navbar />
